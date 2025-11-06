ee675cece2c40b24336b626fb151b7106ec20373
Chris Richardson <Chris@ChrisRichardson.net>
2025-11-06 12:00:23 -0800

    Wait for OSO container to become healthy

--- realguardio-oso-integration-service/start-oso-dev-server.sh
+++ realguardio-oso-integration-service/start-oso-dev-server.sh
@@ -3,6 +3,13 @@
 if [ -z "$(docker ps -q -f ancestor=public.ecr.aws/osohq/dev-server)" ]; then
   docker run -d --rm -p :8080 -v "$PWD/policies:/policies" \
       public.ecr.aws/osohq/dev-server:latest --watch-for-changes /policies/main.polar
+  source ./set-oso-env.sh
+  until curl --fail "${OSO_URL}/health" ; do
+    echo -n .
+    sleep 1
+  done
 else
   echo OSO dev server is already running
 fi
+echo
+echo
